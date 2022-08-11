b8d78b37982aacc450d353fbad109404a609ea5c
Nate B <nathanbrengman@gmail.com>
2022-08-11 17:34:48 -0500

    forgot commas between employees in schema like a total idiot

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,4 @@ INSERT INTO role (title, salary, department_id)
 VALUE ("Accountant", 70000.00, 1),("Human Resources Specialist", 59000.00, 2),("Sales Specialist", 68000.00, 3),("Marketing Specialist", 73000.00, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUE ("Steve", "Buscemi", 1, 4)("Jason", "Momoa", 2, 3)("Chris", "Pratt", 3, 2)("Snoop", "Dogg", 4, 1)
+VALUE ("Steve", "Buscemi", 1, 4), ("Jason", "Momoa", 2, 3), ("Chris", "Pratt", 3, 2), ("Snoop", "Dogg", 4, 1)
